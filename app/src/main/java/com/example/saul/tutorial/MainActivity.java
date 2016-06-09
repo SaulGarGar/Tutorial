45da46a96ba67d28f510625c1a79d00f77237bba
saulgar <saul.alejandro.garcia.garcia@gmail.com>
2016-06-09 18:44:28 -0500

    change

---
+++ app/src/main/java/com/example/saul/tutorial/MainActivity.java
@@ -0,0 +1,71 @@
+package com.example.saul.tutorial;
+
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import java.util.logging.Handler;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ImageView view;
+    AnimationDrawable frameAnimation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        view = (ImageView)findViewById(R.id.tutorial);
+        view.setBackgroundResource(R.drawable.animation);
+        frameAnimation = (AnimationDrawable) view.getBackground();
+        frameAnimation.setOneShot(true);
+
+
+
+
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+
+    }
+
+
+}
